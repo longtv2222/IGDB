3622b32516951705f70d85fffef64b4d08e583b3
Takahiro Fujia <takahirotanakaSJB@gmail.com>
2020-12-03 14:19:45 -0700

    Create SQLite.sql

---
+++ backend/SQLite.sql
@@ -0,0 +1,4 @@
+INSERT INTO VIDEO_GAME VALUES ('4', '"We go in and out 30 seconds", Hostage, Biohazard Contianer. Break into or Defend your objective with your life. ', 'Rainbow Six Seiege', 'OUT', 'Ubisoft Montreal');
+SELECT *FROM VIDEO_GAME
+
+
