a28c22116ad3a97103c96c7502c47034cb98a5a2
longtv2222 <long.tv2222@gmail.com>
2020-12-03 15:38:29 -0700

    User api

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 
+
+//Sample code
+/*
 app.get("/api/user", (req, res, next) => {
     var sql = "select * from user"
     var params = []
@@ -120,8 +123,72 @@ app.delete("/api/user/:id", (req, res, next) => {
             res.json({"message":"deleted", rows: this.changes})
     });
 })
+*/ 
 
+app.get("/client", (req, res, next) => {
+    var sql = "SELECT * FROM CLIENT;"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
 
+app.get("/client/:id/:asd", (req, res, next) => {
+    var sql = "SELECT * FROM CLIENT WHERE U_ID = ?;"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "data":row
+        })
+      });
+});
+
+app.post("/client/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.id){
+        errors.push("No user id specified");
+    }
+    var data = {
+        id : req.body.id
+    }
+
+    var sql ='INSERT INTO CLIENT VALUES (?);'
+    var params =[data.id]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "U_ID" : this.lastID
+        })
+    });
+})
+
+app.delete("/client/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM CLIENT WHERE U_ID = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", rows: this.changes})
+    });
+})
 // Root path
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
