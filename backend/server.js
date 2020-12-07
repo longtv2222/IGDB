0512d9813651c51ba83d2beeb170ae9732e2a251
longtv2222 <long.tv2222@gmail.com>
2020-12-07 15:03:20 -0700

    Publishes done

--- backend/server.js
+++ backend/server.js
@@ -15,118 +15,6 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
-
-//Sample code
-/*
-app.get("/api/user", (req, res, next) => {
-    var sql = "select * from user"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
-});
-
-
-app.get("/api/user/:id", (req, res, next) => {
-    var sql = "select * from user where id = ?"
-    var params = [req.params.id]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-
-
-app.post("/api/user/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
-        errors.push("No password specified");
-    }
-    if (!req.body.email){
-        errors.push("No email specified");
-    }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
-        return;
-    }
-
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : md5(req.body.password)
-    }
-
-    var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
-    var params =[data.name, data.email, data.password]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-})
-
-
-
-app.patch("/api/user/:id", (req, res, next) => {
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : req.body.password ? md5(req.body.password) : undefined
-    }
-    db.run(
-        `UPDATE user set 
-           name = coalesce(?,name), 
-           email = COALESCE(?,email), 
-           password = coalesce(?,password) 
-           WHERE id = ?`,
-        [data.name, data.email, data.password, req.params.id],
-        (err, result) => {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({
-                message: "success",
-                data: data
-            })
-    });
-})
-
-
-app.delete("/api/user/:id", (req, res, next) => {
-    db.run(
-        'DELETE FROM user WHERE id = ?',
-        req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({"message":"deleted", rows: this.changes})
-    });
-})
-*/
-
-
 /* ******************* PAID USER OPERATIONS  ******************* */
 app.post("/client/paid_user/", (req, res) => {
     var errors = []
@@ -582,8 +470,75 @@ app.delete("/publisher/:pname/plocation_table/", (req, res, next) => {
         });
 })
 
+/*****************************************PUBLISHES**************** */
+app.get("/publisher/publishes", (req, res) => {
+    var sql = "SELECT * FROM PUBLISHES NATURAL JOIN VIDEO_GAME;"
+    db.all(sql, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
+app.get("/publisher/:pname/publishes", (req, res) => {
+    var sql = "SELECT * FROM PUBLISHES NATURAL JOIN VIDEO_GAME WHERE PNAME = ?;"
+
+    db.get(sql, req.params.pname, (err, row) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "data": row
+        })
+    });
+});
+
+app.post("/publisher/:pname/publishes", (req, res) => {
+    var data = {
+        pname: req.params.pname,
+        v_id : req.body.v_id
+    }
+
+    var sql = 'INSERT INTO PUBLISHES VALUES (?, ?);'
+    var params = [data.pname, data.v_id]
+    db.run(sql, params, function (err) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data
+        })
+    });
+})
+
+app.delete("/publisher/:pname/publishes", (req, res) => {
+    var data = {
+        pname : req.params.pname,
+        v_id : req.body.v_id
+    }
+    var params = [data.pname, data.v_id];
+    db.run(
+        'DELETE FROM PUBLISHES WHERE PNAME = ? AND V_ID = ?',
+        params,
+        function (err) {
+            if (err) {
+                res.status(400).json({ "error": res.message })
+                return;
+            }
+            res.json({ "message": "deleted", rows: this.changes })
+        });
+})
+
 // //////////////////////////////////PUBLISHER////////////////////////////////////////////////////////////////////////
-app.get("/publisher", (req, res) => {
+app.get("/publisherS", (req, res) => {
     var sql = "SELECT * FROM PUBLISHER;"
     db.all(sql, (err, rows) => {
         if (err) {
@@ -638,7 +593,7 @@ app.delete("/publisher/:pname", (req, res) => {
     db.run(
         'DELETE FROM PUBLISHER WHERE pname = ?',
         req.params.pname,
-        function (err, result) {
+        function (err) {
             if (err) {
                 res.status(400).json({ "error": res.message })
                 return;
@@ -646,6 +601,9 @@ app.delete("/publisher/:pname", (req, res) => {
             res.json({ "message": "deleted", rows: this.changes })
         });
 })
+
+
+
 
 
 
