ad3c04382937fcb40f5835f3c3ce583f84f001af
Takahiro Fujia <takahirotanakaSJB@gmail.com>
2020-12-07 17:45:23 -0700

    Update Participate.js

--- backend/Participate.js
+++ backend/Participate.js
@@ -1,81 +1,69 @@
 
-app.get("/Participate", (req, res, next) => {
-    var sql = "SELECT * FROM PARTICIPATE;"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
-});
-/////////////////////THIS NEEDS REVIEW
-app.get("/Participate/:CName/:PlayerName/:League", (req, res, next) => {
-    var sql = "SELECT * FROM PARTICIPATE WHERE CNAME = ? AND PLAYERNAME = ? AND LEAGUE = ?;"
-    var params = [req.params.id]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "data":row
-        })
-      });
-});
 
-///THIS/////////////////////////////NEEDS REVIEW
-app.post("/Participate/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.id){
-        errors.push("No CName key specified");
-    }
-    var data = {
-        CName : req.body.id
-		PlayerName : req.body.PlayerName
-		League : req.body.League
-    }
-
-    var sql ='INSERT INTO PARTICIPATE(CNAME, PLAYERNAME, LEAGUE) VALUES (?, ?, ?);'
-    var params =[data.CName, data.PlayerName, data.League]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
+/*****************************************PARTICIPATE**************** */
+app.get("/Competition/Partcipate", (req, res) => {
+    var sql = "SELECT * FROM PARTICIPATE NATURAL JOIN PLAYER;"
+    db.all(sql, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
             return;
         }
         res.json({
             "message": "success",
-             "id" : this.lastID
-			 "data" : data
+            "data": rows
+        })
+    });
+});
+
+app.get("/Competition/:CName/Partcipate", (req, res) => {
+    var sql = "SELECT * FROM PARTICIPATE NATURAL JOIN PLAYER WHERE CNAME = ?;"
+
+    db.all(sql, req.params.pname, (err, row) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "data": row
+        })
+    });
+});
+
+app.post("/Competition/:CName/Partcipate", (req, res) => {
+    var data = {
+        CName: req.params.CName,
+        PlayerName : req.body.PlayerName,
+		League: req.body.League
+    }
+
+    var sql = 'INSERT INTO PUBLISHES VALUES (?, ?, ?);'
+    var params = [data.CName, data.PlayerName, data.League]
+    db.run(sql, params, function (err) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data
         })
     });
 })
 
-////THIS//////////////NEEDS REVIEW
-app.delete("/Participate/:CName/:PlayerName/:League", (req, res, next) => {
+app.delete("/Competition/:CName/Partcipate", (req, res) => {
+    var data = {
+         CName: req.params.CName,
+		 PlayerName : req.body.PlayerName
+    }
+    var params = [data.CName, data.PlayerName;
     db.run(
-        'DELETE FROM PARTICIPATE WHERE CNAME = ? AND PLAYERNAME = ? AND LEAGUE = ?',
-        req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
+        'DELETE FROM PUBLISHES WHERE CName = ? AND PlayerName = ?',
+        params,
+        function (err) {
+            if (err) {
+                res.status(400).json({ "error": res.message })
                 return;
             }
-            res.json({"message":"deleted", rows: this.changes})
-    });
+            res.json({ "message": "deleted", rows: this.changes })
+        });
 })
-
-
-
-
-///////////////////////////////////////////////////////////
-
-
-// Root path
-app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
-});
