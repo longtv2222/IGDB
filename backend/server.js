818f97b18b3a70aee62a13e2433cb7ed25d953a7
longtv2222 <long.tv2222@gmail.com>
2020-12-11 02:37:20 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 var express = require("express")
 var app = express()
-var db = require("./database.js")
 var bodyParser = require("body-parser");
-const { Statement, Database } = require("sqlite3");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var HTTP_PORT = 8000
+
 // Start server
+var HTTP_PORT = 8000
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
