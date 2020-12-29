41950c9d86fca8c55c3cd85d8419016f9ad313bf
longtv2222 <long.tv2222@gmail.com>
2020-12-28 22:33:23 -0700

    Code refactoring

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ const bodyParser = require("body-parser")
 const authentication = require('./middleware/authentication')
 const connection = require('./cloudDatabase')
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;    //For debugging purpose only, need to be fixed
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;    //For debugging purpose only, need to be fixed
 // Start server
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+
 var HTTP_PORT = 8000
 app.listen(HTTP_PORT, () => {
-  // console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
+  console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
 const signer = connection.signInAWS()
@@ -29,16 +31,6 @@ client.query('SELECT * FROM BYE;', (err, res) => {
   }
 })
 
-const clientRoutes = require("./routes/client");
-const developerRoutes = require("./routes/developer");
-const videoGameRoutes = require('./routes/Video_Game');
-const publisherRoutes = require('./routes/publisher');
-const playerRoutes = require('./routes/player');
-const teamRoutes = require('./routes/team');
-const competitionRoutes = require('./routes/competition');
-const esportRoutes = require('./routes/esport');
-
-
 //Authentication middleware for all post
 app.post('*', authentication, (res, req, next) => {
   next();
@@ -54,13 +46,28 @@ app.patch('*', authentication, (res, req, next) => {
   next();
 })
 
+const clientRoutes = require("./routes/client");
 app.use('/client', clientRoutes);
+
+const developerRoutes = require("./routes/developer");
 app.use('/developer', developerRoutes);
+
+const videoGameRoutes = require('./routes/Video_Game');
 app.use('/Video_Game', videoGameRoutes);
+
+const publisherRoutes = require('./routes/publisher');
 app.use('/publisher', publisherRoutes);
+
+const playerRoutes = require('./routes/player');
 app.use('/player', playerRoutes);
+
+const teamRoutes = require('./routes/team');
 app.use('/team', teamRoutes);
+
+const competitionRoutes = require('./routes/competition');
 app.use('/competition', competitionRoutes);
+
+const esportRoutes = require('./routes/esport');
 app.use('/esport', esportRoutes)
 
 app.get('/', (req, res) => {
