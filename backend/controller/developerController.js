5e36059804bdb89fdc334352c976e6879ada6afc
longtv2222 <long.tv2222@gmail.com>
2021-01-07 22:01:01 -0700

    Developer endpoints done

--- backend/controller/developerController.js
+++ backend/controller/developerController.js
@@ -1,169 +1,115 @@
 const db = require("../db/database.js")
 
-exports.getADeveloper = (req, res) => {
-    var sql = "SELECT * FROM DEVELOPER WHERE DNAME = ?;"
-    db.get(sql, req.params.dname, (err, row) => {
-        if (err) {
-            res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "data": row
-        })
-    });
+const { pool } = require('../db/cloudDatabase')
+
+
+
+exports.getADeveloper = async (req, res) => {
+    const sql = "SELECT * FROM DEVELOPER WHERE DNAME = $1;"
+    try {
+        const { rows } = await pool.query(sql, req.params.dname);
+        res.json(rows);
+    } catch (error) {
+        res.json(error.stack);
+    }
 }
 
-exports.postADeveloper = (req, res) => {
-    var errors = []
-    if (!req.body.DName) {
-        errors.push("No developer name is specified");
+exports.postADeveloper = async (req, res) => {
+    const sql = 'INSERT INTO DEVELOPER VALUES ($1);'
+    try {
+        await pool.query(sql, req.body.dname);
+        res.json({ message: 'Insert developer ' + req.body.dname + ' succefully.' })
+    } catch (error) {
+        res.json(error.stack);
+    }
+}
+
+exports.getAllDeveloper = async (_req, res) => {
+    try {
+        const { rows } = await pool.query('SELECT * FROM DEVELOPER;');
+        res.json(rows);
+    } catch (error) {
+        res.json(error.stack);
     }
 
-    var sql = 'INSERT INTO DEVELOPER VALUES (?);'
-    var data = req.body.DName
-    db.run(sql, data, (err, result) => {
-        if (err) {
-            res.status(400).json({ "error": err.message })
-            return;
-        }
-        res.json({
-            "DName": data
-        })
-    });
 }
 
-exports.getAllDeveloper = (req, res) => {
-    var sql = "SELECT * FROM DEVELOPER;"
-    db.all(sql, (err, row) => {
-        if (err) {
-            res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "data": row
-        })
-    });
-}
-
-exports.deleteADeveloper = (req, res) => {
-    db.run(
-        'DELETE FROM DEVELOPER WHERE DName = ?;',
-        req.params.dname,
-        function (err, result) {
-            if (err) {
-                res.status(400).json({ "error": res.message })
-                return;
-            }
-            res.json({ "message": "deleted", rows: this.changes })
-        });
-}
-
-exports.getLocationWithDName = (req, res, next) => {
-    var sql = "SELECT * FROM DLOCATION_TABLE WHERE DName = ?;"
-    var params = [req.params.dname]
-    db.all(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "data": row
-        })
-    });
-}
-
-exports.postLocationWithDName = (req, res, next) => {
-    var errors = []
-    if (!req.body.dname) {
-        errors.push("No developer name is specified");
+exports.deleteADeveloper = async (req, res) => {
+    try {
+        await pool.query('DELETE FROM DEVELOPER WHERE DNAME = $1', req.params.dname);
+        res.json('DELETED developer with name ' + req.params.dnmae);
+    } catch (error) {
+        res.json(error.stack);
     }
-
-    var sql = 'INSERT INTO DLOCATION_TABLE VALUES(?,?);'
-    var data = {
-        u_id: req.params.dname,
-        location: req.body.location
-    }
-
-    var params = [data.u_id, data.location]
-    db.run(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ "error": err.message })
-            return;
-        }
-        res.json({
-            data
-        })
-    });
 }
 
-exports.deleteLocation = (req, res, next) => {
-    var data = {
-        dname: req.params.dname,
-        location: req.params.location
+exports.getLocationWithDName = async (req, res) => {
+    const sql = "SELECT * FROM DLOCATION_TABLE WHERE DName = $1;"
+    try {
+        const { rows } = await pool.query(sql, req.params.dname);
+        res.json(rows);
+    } catch (error) {
+        res.json(error.stack);
     }
-
-    var params = [data.dname, data.location]
-    db.run(
-        'DELETE FROM DLOCATION_TABLE WHERE DName = ? AND LOCATION = ?;',
-        params,
-        function (err, result) {
-            if (err) {
-                res.status(400).json({ "error": res.message })
-                return;
-            }
-            res.json({ "message": "deleted", rows: this.changes })
-        });
 }
 
-exports.getAllVideoGamesWithDName = (req, res) => { //All video games this developer develops
-    var sql = "SELECT * FROM DEVELOPS AS D NATURAL JOIN VIDEO_GAME WHERE D.DNAME = ?;"
-    var params = [req.params.dname]
+exports.postLocationWithDName = async (req, res) => {
+    const sql = 'INSERT INTO DLOCATION_TABLE VALUES($1, $2);'
+    const params = [req.params.dname, req.body.location]
 
-    db.get(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "data": row
-        })
-    });
+    try {
+        await pool.query(sql, params);
+        res.json({ message: 'Insert succefully' })
+    } catch (error) {
+        res.json(error.stack);
+    }
 }
 
-exports.postADevelop = (req, res) => {
-    var data = {
-        v_id: req.body.v_id,
-        dname: req.params.dname
-    }
+exports.deleteLocation = async (req, res) => {
 
-    var sql = 'INSERT INTO DEVELOPS VALUES (?,?);'
-    var params = [data.v_id, data.dname]
-    db.run(sql, params, function (err, result) {
-        if (err) {
-            res.status(400).json({ "error": err.message })
-            return;
-        }
-        res.json({
-            "message": "success",
-            "U_ID": data
-        })
-    });
+    const sql = 'DELETE FROM DLOCATION_TABLE WHERE DNAME = $1 AND LOCATION = $2;'
+    const params = [req.params.dname, req.params.location]
+
+    try {
+        await pool.query(sql, params);
+        res.json({ message: 'Deleted succefully' })
+    } catch (error) {
+        res.json(error.stack);
+    }
 }
 
-exports.deleteADevelop = (req, res) => {
-    var data = {
-        v_id: req.body.v_id,
-        dname: req.params.dname
+exports.getAllVideoGamesWithDName = async (req, res) => { //All video games this developer develops
+    const sql = "SELECT * FROM DEVELOPS AS D NATURAL JOIN VIDEO_GAME WHERE D.DNAME = $1;"
+    const params = [req.params.dname]
+    try {
+        const { rows } = await pool.query(sql, params);
+        res.json(rows);
+    } catch (error) {
+        res.json(error.stack);
     }
-    var params = [data.v_id, data.dname]
-    db.run(
-        'DELETE FROM DEVELOPS WHERE V_ID = ? AND DNAME = ?',
-        params,
-        function (err, result) {
-            if (err) {
-                res.status(400).json({ "error": res.message })
-                return;
-            }
-            res.json({ "message": "deleted", rows: this.changes })
-        });
+}
+
+exports.postADevelop = async (req, res) => {
+
+
+    const sql = 'INSERT INTO DEVELOPS VALUES ($1, $2);'
+    const params = [req.body.v_id, req.params.dname]
+
+    try {
+        await pool.query(sql, params);
+        res.json('INSERTED SUCCEFULLY');
+    } catch (error) {
+        res.json(error.stack);
+    }
+}
+
+exports.deleteADevelop = async (req, res) => {
+    const params = [req.body.v_id, , req.params.dname]
+    const sql = 'DELETE FROM DEVELOPS WHERE V_ID = $1 AND DNAME = $2;'
+    try {
+        await pool.query(sql, params);
+        res.json({ message: 'DELETED SUCCEFULLY' })
+    } catch (error) {
+        res.json(error.stack);
+    }
 }
