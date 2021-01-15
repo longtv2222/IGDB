75e21a59083c5d4fdb739ff01bc743e4e7bc2cc2
longtv2222 <long.tv2222@gmail.com>
2021-01-15 14:33:55 -0700

    Finished esport

--- backend/controller/esportController.js
+++ backend/controller/esportController.js
@@ -4,9 +4,9 @@ const { pool } = require('../db/cloudDatabase')
 exports.getAllEsport = async (_req, res) => {
     try {
         const { rows } = await pool.query('SELECT * FROM ESPORT;');
-        res.json(rows);
+        res.status(200).json(rows);
     } catch (error) {
-        res.json(error.stack);
+        res.status(500).json(error.stack);
     }
 }
 
@@ -14,17 +14,17 @@ exports.postAEsport = async (req, res) => {
     const sql = 'INSERT INTO ESPORT VALUES ($1);'
     try {
         await pool.query(sql, [req.body.league])
-        res.json({ message: 'Insert succefully' });
+        res.status(200).json({ message: 'Insert succefully' });
     } catch (error) {
-        res.json(error.stack);
+        res.status(500).json(error.stack);
     }
 }
 
 exports.deleteALeague = async (req, res) => {
     try {
         await pool.query('DELETE FROM ESPORT WHERE LEAGUE = $1;', [req.params.league]);
-        res.json({ message: 'Delete succesfully' })
+        res.status(200).json({ message: 'Delete succesfully' })
     } catch (error) {
-        res.json(error.stack);
+        res.status(500).json(error.stack);
     }
 }
--- backend/routes/esport.js
+++ backend/routes/esport.js
@@ -2,11 +2,93 @@ const router = require('express').Router();
 const esportController = require('../controller/esportController')
 
 
-/************************ESPORT******************** */
+/**
+ * @swagger
+ * /esport :
+ *    get:
+ *      tags:
+ *      - "esport"
+ *      summary : Get all esports
+ *      responses :
+ *        200:
+ *          description : Get all esports succesfully
+ *        500:
+ *          description : Error occured        
+ */
 router.get("/", esportController.getAllEsport);
 
+/**
+ * @swagger
+ * /esport :
+ *    post:
+ *      tags:
+ *      - "esport"
+ *      security:
+ *      - ApiKeyAuth: []
+ *      summary : Insert an esport
+ *      parameters :
+ *          - name : esport
+ *            in : body
+ *            description : esport
+ *            schema :
+ *              type : object
+ *              required : league
+ *              properties :
+ *                  league :
+ *                      type : string
+ *                      example : "LOL ESPORT"
+ *      responses :
+ *        200:
+ *          description : Insert esport succesfully
+ *        500:
+ *          description : Error occured
+ *        401:
+ *          description : Error occured 
+ *          responseBody :
+ *          content :
+ *              application/json:
+ *                  schema : 
+ *                      type : object
+ *                      properties :
+ *                          message :
+ *                              type : string
+ *                      example :
+ *                          message : "Authentication failed"           
+ */
 router.post("/", esportController.postAEsport);
 
+/**
+ * @swagger
+ * /esport/{league} :
+ *    delete:
+ *      tags:
+ *      - "esport"
+ *      security:
+ *      - ApiKeyAuth: []
+ *      summary : Delete an esport
+ *      parameters :
+ *          - name : esport
+ *            in : path
+ *            description : esport
+ *            example : "League of Legend Esport"
+ *      responses :
+ *        200:
+ *          description : Delete specified esport succesfully
+ *        500:
+ *          description : Error occured
+ *        401:
+ *          description : Error occured 
+ *          responseBody :
+ *          content :
+ *              application/json:
+ *                  schema : 
+ *                      type : object
+ *                      properties :
+ *                          message :
+ *                              type : string
+ *                      example :
+ *                          message : "Authentication failed"           
+ */
 router.delete("/:league", esportController.deleteALeague);
 
 module.exports = router;
