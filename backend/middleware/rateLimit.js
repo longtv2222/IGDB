14c43d599fc9742f00acfde32066f39d1e2c5ef9
longtv2222 <long.tv2222@gmail.com>
2021-01-24 22:41:55 -0700

    Update

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -1,12 +1,17 @@
 const rateLimit = require("express-rate-limit");
 
+var keyGenerator1 = function (req /*, res*/) {
+  return req.headers['x-real-ip']
+}
+
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
 // app.set('trust proxy', 1);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
+  keyGenerator : keyGenerator1,
 });
 
 module.exports = limiter;
